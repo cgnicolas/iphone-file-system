9014a74055392c85fcf285f524dda61cd36a1da4
cgnicolas <cgnicolas@gmail.com>
2019-04-03 16:50:22 -0400

    Renderer complete

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   // Create the browser window.
-  let win = new BrowserWindow({ width: 400, height: 700, maxWidth: 400, maxHeight: 700 })
-
+  let win = new BrowserWindow({ 
+    width: 400,
+    height: 700, 
+    // maxHeight: 700, 
+    // maxWidth: 400, 
+    // minHeight:700, 
+    // minWidth: 400
+  })
   // and load the index.html of the app.
+  win.setMenu(null);
   win.loadFile('index.html')
 }
 
--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const PIXI = require('pixi.js')
 const fs = require('fs'); 
+const PIXI = require('pixi.js')
+let canvas = document.getElementById('myCanvas')
+const app = new PIXI.Application({
+    view: canvas,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: 0x061639
+})
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
